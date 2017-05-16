edf711c34b659b4e795efe66187aa7f24b8469d5
FuChangsheng <15527941667@163.com>
2017-05-16 11:18:57 +0800

    commit

--- src/process/config.py
+++ src/process/config.py
@@ -1,6 +1,6 @@
 
-#api_key = 's1g0b64347fU2jeATtOkMcCipayWnuEGdd8CTqFs'
-api_key='31X4u9A3k7O9O626c9x7LlEWBz1IyVjcAsDfnotq'
+api_key = 's1g0b64347fU2jeATtOkMcCipayWnuEGdd8CTqFs'
+# api_key='31X4u9A3k7O9O626c9x7LlEWBz1IyVjcAsDfnotq'
 
-# url = 'http://api.ltp-cloud.com/analysis/'
-url = 'http://ltpapi.voicecloud.cn/analysis/'
+url = 'http://api.ltp-cloud.com/analysis/'
+# url = 'http://ltpapi.voicecloud.cn/analysis/'
--- src/process/process.py
+++ src/process/process.py
@@ -6,14 +6,14 @@ def ws(text):
     param = {
         'api_key': config.api_key,
         'text': text,
-        'pattern': 'all',
-        'format': 'conll'
+        'pattern': 'ws',
+        'format': 'xml'
     }
 
     r = requests.post(config.url, param)
     r.encoding = 'utf-8'
     return r.text
 
-s = '我今天在学校吃了一只非常大的鸡腿和一个冰淇淋，因为我饿了'
+s = '我是信息学院的学生。'
 print(ws(s))
 
